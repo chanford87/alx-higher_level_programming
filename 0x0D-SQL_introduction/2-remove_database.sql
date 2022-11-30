21d7cb8b280a5142309286dd01450d57507dd5da
chanford87 <hermannchanford@yahoo.fr>
2022-11-29 22:16:00 -0800

    fix task2 againt

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
---deletes the database hbtn_0c_0 in your MySQL server.
---do nothing if the database hbtn_0c_0 not exist
+-- deleting a database
+-- should not fail if database doesn't exist
 DROP DATABASE IF EXISTS hbtn_0c_0
